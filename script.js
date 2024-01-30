531f5fe4be206826ebb5b1ab994f0fca05f39c7c
Krushna Bawalge <65840834+krushnkumar666@users.noreply.github.com>
2024-01-30 23:30:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".multiple-text", {
+  strings: ["Web Developer", "Frontend Developer", "Full Stack Developer"],
+  typeSpeed: 50,
+  backSpeed: 40,
+  backDelay: 800,
+  loop: true,
+});
+
+
